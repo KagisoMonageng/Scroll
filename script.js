3911ac0a7d6961790f0f07dd21e5e655fc705312
KagisoMonageng <63680048+KagisoMonageng@users.noreply.github.com>
2023-04-04 23:26:09 +0200

    added scroll

---
+++ script.js
@@ -0,0 +1,14 @@
+// // select video element
+// var vid = document.getElementById('v0');
+// //keep video paused until scroll
+// vid.pause();
+// window.onscroll = function(){
+//     vid.pause();
+// };
+
+// setInterval(function(){
+//     vid.currentTime = window.pageYOffset/400;
+// }, 40);
+
+// all above from first attempt
+
